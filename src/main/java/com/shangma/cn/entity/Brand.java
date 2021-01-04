1aa2519c413ff6dd1aadd313076719e718ea82e9
wzy <1304847504@qq.com>
2021-01-04 17:17:05 +0800

    逆向工程生成完成 代码配置完成

---
+++ src/main/java/com/shangma/cn/entity/Brand.java
@@ -0,0 +1,95 @@
+package com.shangma.cn.entity;
+
+import java.util.Date;
+
+public class Brand {
+    private String id;
+
+    private String brandName;
+
+    private String brandSite;
+
+    private String brandDesc;
+
+    private String brandLogo;
+
+    private Date addTime;
+
+    private Long creatorId;
+
+    private Date updateTime;
+
+    private Long updateId;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id == null ? null : id.trim();
+    }
+
+    public String getBrandName() {
+        return brandName;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName == null ? null : brandName.trim();
+    }
+
+    public String getBrandSite() {
+        return brandSite;
+    }
+
+    public void setBrandSite(String brandSite) {
+        this.brandSite = brandSite == null ? null : brandSite.trim();
+    }
+
+    public String getBrandDesc() {
+        return brandDesc;
+    }
+
+    public void setBrandDesc(String brandDesc) {
+        this.brandDesc = brandDesc == null ? null : brandDesc.trim();
+    }
+
+    public String getBrandLogo() {
+        return brandLogo;
+    }
+
+    public void setBrandLogo(String brandLogo) {
+        this.brandLogo = brandLogo == null ? null : brandLogo.trim();
+    }
+
+    public Date getAddTime() {
+        return addTime;
+    }
+
+    public void setAddTime(Date addTime) {
+        this.addTime = addTime;
+    }
+
+    public Long getCreatorId() {
+        return creatorId;
+    }
+
+    public void setCreatorId(Long creatorId) {
+        this.creatorId = creatorId;
+    }
+
+    public Date getUpdateTime() {
+        return updateTime;
+    }
+
+    public void setUpdateTime(Date updateTime) {
+        this.updateTime = updateTime;
+    }
+
+    public Long getUpdateId() {
+        return updateId;
+    }
+
+    public void setUpdateId(Long updateId) {
+        this.updateId = updateId;
+    }
+}
